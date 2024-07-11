218942cb2971fdafd9f7f5a18a10689adfeb009e
Subhajit Bokshi <52731122+Subhajitb17@users.noreply.github.com>
2024-07-11 10:52:34 +0530

    env setup

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,13 +1,13 @@
 require("dotenv").config({
-    path:
-      process.env.NODE_ENV === "production"
-        ? ".env.production"
-        : ".env.development",
-  });
-  
-  const config = {
-    PORT: process.env.PORT,
-    DATABASE_URI: process.env.DATABASE_URI,
-  };
-  
-  module.exports = config;
+  path:
+    process.env.NODE_ENV === "production"
+      ? ".env.production"
+      : ".env.development",
+});
+
+const config = {
+  PORT: process.env.PORT,
+  DATABASE_URI: process.env.DATABASE_URI,
+};
+
+module.exports = config;
